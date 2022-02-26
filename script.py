af4bfb6fa3a22cf8124780990473c500f6a6a6ac
vmenon <vmenon48@gatech.edu>
2022-02-25 21:57:24 -0600

    procfile

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
-import time
+from flask import Flask, render_template
+app = Flask(__name__, static_folder='static') 
 
-while True:
-	print("I am working!")
-	time.sleep(2)
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+if __name__ == '__main__':
+    from os import environ
+    app.run(debug=False, port=environ.get("PORT", 5000),host=localhost)
