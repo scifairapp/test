b4bdc16694b037e2f213464f2c494b979d947d71
vmenon <vmenon48@gatech.edu>
2022-02-25 22:33:54 -0600

    procfile

--- script.py
+++ script.py
@@ -7,5 +7,5 @@ def hello_world():
 
 if __name__ == '__main__':
     from os import environ
-    port = int(os.environ.get('PORT', 5000))
+    port = int(environ.get('PORT', 5000))
     app.run(debug=False, port=port,host='0.0.0.0')
